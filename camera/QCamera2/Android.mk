71b465c818de14eeb07dc08582c025f8243ef5fe
Jignesh Mehta <jigneshm@codeaurora.org>
2015-06-27 19:04:59 -0700

    QCamera2: Migrate daemon process to mediaserver
    - mci will open and call mct shimlayer APIs.
    - Domain socket connection is removed.
    - Virtual address of hal pointer will be assigned to
      MCT image buffer list.
    - For VB2 operation, it will call to camera kernel.
    
    CRs-Fixed: 1005685
    Change-Id: I2e3edc47148ea2aefc201057695f08ceb4ec070a

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -106,6 +106,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
+LOCAL_SHARED_LIBRARIES += libcutils libdl
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
