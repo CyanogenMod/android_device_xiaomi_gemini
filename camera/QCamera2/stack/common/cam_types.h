f4e2ce48571ff5dd2118b59b576de4959921c823
Chi Zhang <chiz@codeaurora.org>
2016-08-15 18:52:57 +0800

    QCamera2:HAL1:output fv and spot light detect result
    Feature:
    Export each frame's FV and spot light detect result
    Fill metadata with focus value and spot light detection
    results output from af core.
    
    CRs-fixed: 1056159
    Change-Id: Ia0d0f5ac098eeb3a9be046f9d86e1d5a779f8038
    Signed-off-by: Sridhar Gujje <sgujje@qti.qualcomm.com>

--- camera/QCamera2/stack/common/cam_types.h
+++ camera/QCamera2/stack/common/cam_types.h
@@ -1345,6 +1345,8 @@ typedef struct {
     cam_focus_mode_type focus_mode;        /* focus mode from backend */
     int32_t focus_pos;
     cam_af_flush_info_t flush_info;
+    float focus_value;
+    uint8_t spot_light_detected;
 } cam_auto_focus_data_t;
 
 typedef struct {
@@ -2166,6 +2168,10 @@ typedef enum {
     CAM_INTF_PARM_JPEG_ENCODE_CROP,
     /*Param for updating Quadra CFA mode */
     CAM_INTF_PARM_QUADRA_CFA,
+    /*Focus value output from af core*/
+    CAM_INTF_META_FOCUS_VALUE,
+    /*Spot light detection result output from af core*/
+    CAM_INTF_META_SPOT_LIGHT_DETECT,
     CAM_INTF_PARM_MAX
 } cam_intf_parm_type_t;
 
